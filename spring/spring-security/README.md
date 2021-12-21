d3d8c4043663d962a312956fa14699d4e190e3be
hyungjungoo <9h0jun1115@gmail.com>
2021-12-21 12:19:00 +0900

    2021-12-21 add spring security

---
+++ spring/spring-security/README.md
@@ -0,0 +1,7 @@
+# Spring Security
+
+## Objectives
+- Basic configuration with spring-security
+- Use spring security to build simple application configuring authorizations
+- Study the basic architecure of spring security
+- TODO
---
+++ spring/spring-security/src/test/java/me/hyungjun/demospringsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyungjun.demospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
